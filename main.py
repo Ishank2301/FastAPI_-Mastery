a1434b8db3afce5b51d32a9651b2e4f8a76ab22e
Ishank <ishankmishra579@gmil.com>
2026-06-01 20:08:31 +0530

    feat: add readme to dir

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+
+
+@app.get("/")
+def hello():
+    return {"message": "Hello World"}
+
+
+@app.get("/about")
+def about():
+    return {"meassage": "This is an Practise tut for FastAPI "}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
